ee86a6611654e48d665211e2ac58991f35504813
root <1029516758@qq.com>
2020-08-25 15:55:21 +0800

    第八次，查看帖子详情（不能查看评论回复）

--- src/main/java/com/nowcoder/community/controller/DiscussPostController.java
+++ src/main/java/com/nowcoder/community/controller/DiscussPostController.java
@@ -3,11 +3,14 @@ package com.nowcoder.community.controller;
 import com.nowcoder.community.entity.DiscussPost;
 import com.nowcoder.community.entity.User;
 import com.nowcoder.community.service.DiscussPostService;
+import com.nowcoder.community.service.UserService;
 import com.nowcoder.community.util.CommunityUtil;
 import com.nowcoder.community.util.HostHolder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -22,6 +25,9 @@ public class DiscussPostController {
 
     @Autowired
     private HostHolder hostHolder;
+
+    @Autowired
+    private UserService userService;
 
     @RequestMapping(path = "/add",method = RequestMethod.POST)
     @ResponseBody
@@ -41,5 +47,22 @@ public class DiscussPostController {
 
         //以上步骤也有可能报错，以后统一处理
         return CommunityUtil.getJSONString(0,"发布成功");
+    }
+
+    @RequestMapping(path = "/detail/{discussPostId}",method = RequestMethod.GET)
+    public String getDiscussPost(@PathVariable("discussPostId") int discussPostId, Model model){
+
+
+        //帖子
+        DiscussPost discussPost = discussPostService.findDiscussPostById(discussPostId);
+        model.addAttribute("post",discussPost);
+        //作者查询
+        User user = userService.findUserById(discussPost.getUserId());
+        model.addAttribute("user",user);
+
+
+        //帖子的回复和赞后续开发
+
+        return "/site/discuss-detail";
     }
 }
--- src/main/java/com/nowcoder/community/service/DiscussPostService.java
+++ src/main/java/com/nowcoder/community/service/DiscussPostService.java
@@ -43,5 +43,9 @@ public class DiscussPostService {
         return discussPostMapper.insertDiscussPost(discussPost);
     }
 
+    public DiscussPost findDiscussPostById(int id){
+        return discussPostMapper.selectDiscussPostById(id);
+    }
+
 
 }
