6764cd5504c940f0a56f932b6ba8df104eba59d3
root <1029516758@qq.com>
2020-08-28 00:47:22 +0800

    第12次，查看私信列表&查看私信详情

--- src/test/java/com/nowcoder/community/MapperTests.java
+++ src/test/java/com/nowcoder/community/MapperTests.java
@@ -4,9 +4,11 @@ package com.nowcoder.community;
 import com.nowcoder.community.controller.LoginTicket;
 import com.nowcoder.community.dao.DiscussPostMapper;
 import com.nowcoder.community.dao.LoginTicketMapper;
+import com.nowcoder.community.dao.MessageMapper;
 import com.nowcoder.community.dao.UserMapper;
 import com.nowcoder.community.entity.DiscussPost;
 import com.nowcoder.community.entity.User;
+import com.nowcoder.community.entity.Message;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -92,5 +94,32 @@ public class MapperTests {
         loginTicketMapper.updateStatus("qweasd",1);
         loginTicket = loginTicketMapper.selectByTicket("qweasd");
         System.out.println(loginTicket);
+    }
+
+    @Autowired
+    private MessageMapper messageMapper;
+
+    @Test
+    public void testMessageMapper(){
+
+        List<Message> list = messageMapper.selectConversations(111,0,100);
+        for(Message message : list) {
+            System.out.println(message);
+        }
+        int count = messageMapper.selectConversationCount(111);
+        System.out.println(count);
+
+        list = messageMapper.selectLetters("111_112", 0, 10);
+        for (Message message: list){
+                System.out.println(message);
+        }
+
+        count = messageMapper.selectLetterCount("111_112");
+        System.out.println(count);
+
+
+        count = messageMapper.selectLetterUnreadCount(131, "111_131");
+        System.out.println(count);
+
     }
 }
